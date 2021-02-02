d240feb5b287656c60ab145f30ba797764d45e2e
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-02-02 03:23:50 +0000

    updated head.js

--- head.js
+++ head.js
@@ -1,11 +1,21 @@
+
+
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("✅✅✅" + `Assertation Passed: ${actual} === ${expected});
+    console.log("✅✅✅" + `Assertation Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("🛑🛑🛑" + `Assertation Failed: + ${actual} + "!==" + ${expected});
+    console.log("🛑🛑🛑" + `Assertation Failed: ${actual} !== ${expected}`);
   }
 };
-console.log("Hello World")
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+const head = function(array) {
+   
+return array[0]
+
+}
+
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
