3fc8fa85a772fa2d47ef69c331c73e5313676d37
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-02-03 21:09:00 +0000

    adding middle js

---
+++ middle.js
@@ -0,0 +1,64 @@
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+
+    console.log("✅✅✅" + `Assertation Passed: ${actual} === ${expected}`);
+  } else {
+    console.log("🛑🛑🛑" + `Assertation Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+  for (i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+
+      return false
+    }
+
+
+  }
+  return true
+
+}
+
+
+const middle = function (array) {
+
+  let num = [];
+
+  if (array.length < 3) {
+    return num
+  }
+
+  else if (array.length % 2 === 1) {
+
+    num = array[Math.floor((array.length - 1) / 2)];
+    return num
+
+  } else if (array.length % 2 === 0) {
+    num[0] = array[(array.length / 2) - 1];
+    num[1] = array[array.length / 2];
+
+    return num
+
+
+  }
+
+};
+
+
+
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));
+
+
+console.log(middle([1, 2, 3, 4]))
+console.log(middle([1, 2, 3, 4, 5, 6]));
+
+console.log(middle([1]));
+console.log(middle([1, 2]));
