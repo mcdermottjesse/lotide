86b2ff4aaff4e197ecfbf5612de4d944ad760e53
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-02-03 06:22:37 +0000

    assertation checking equal arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,37 @@
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+
+    console.log("✅✅✅" + `Assertation Passed: ${actual} === ${expected}`);
+  } else {
+    console.log("🛑🛑🛑" + `Assertation Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+  for (i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+
+      return false
+    }
+
+
+  }
+  return true
+
+}
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); 
+
+
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3]); 
+
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
